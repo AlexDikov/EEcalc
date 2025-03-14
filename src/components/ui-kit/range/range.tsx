cb880983d7824a9d589dd25ae0c52548b0cd70a9
Alex Dikov <dikov_alex@mail.ru>
2025-03-14 21:34:13 +0300

    fix arrays and components

--- src/components/ui-kit/range/range.tsx
+++ src/components/ui-kit/range/range.tsx
@@ -4,9 +4,5 @@ import { RangeProps } from '../../../types/range-props';
 export const Range = (props: RangeProps) => {
   const { register } = useFormContext();
 
-  return (
-    <input {...register(props.value)} type="range">
-      {props.name}
-    </input>
-  );
+  return <input {...register(props.name)} type="range" />;
 };
--- src/types/range-props.ts
+++ src/types/range-props.ts
@@ -1,4 +1,3 @@
 export type RangeProps = {
   name: string;
-  value: string;
 };
